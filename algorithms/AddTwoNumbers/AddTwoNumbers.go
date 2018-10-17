0cbf310a4dfbceed4a63aacee134ee352513e1cd
zhangbitao <zhangbitao01@gmail.com>
2018-10-17 23:28:38 +0800

    gofmt

--- algorithms/AddTwoNumbers/AddTwoNumbers.go
+++ algorithms/AddTwoNumbers/AddTwoNumbers.go
@@ -24,33 +24,33 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-    var result *ListNode
-    r := &result
-    remainder := 0
-    for l1 != nil || l2 != nil {   
-        var v1, v2 int
-        if l1 != nil {
-            v1 = l1.Val
-            l1 = l1.Next
-        }
-        if l2 != nil {
-            v2 = l2.Val
-            l2 = l2.Next
-        }
-        n := &ListNode{
-            Val: (remainder + v1 + v2) % 10,
-        }
-        *r = n
-        r = &(n.Next)
-        
+	var result *ListNode
+	r := &result
+	remainder := 0
+	for l1 != nil || l2 != nil {
+		var v1, v2 int
+		if l1 != nil {
+			v1 = l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			v2 = l2.Val
+			l2 = l2.Next
+		}
+		n := &ListNode{
+			Val: (remainder + v1 + v2) % 10,
+		}
+		*r = n
+		r = &(n.Next)
+
 		remainder = (remainder + v1 + v2) / 10
 
-        if remainder > 0 {
-            n := &ListNode{
-                Val: remainder,
-            }
-            *r = n
-        }
-    }
-    return result
+		if remainder > 0 {
+			n := &ListNode{
+				Val: remainder,
+			}
+			*r = n
+		}
+	}
+	return result
 }
